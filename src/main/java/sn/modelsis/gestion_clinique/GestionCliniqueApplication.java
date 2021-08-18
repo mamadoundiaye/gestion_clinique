f404110bcc9ee200707fa6ff574772c9af8dddbe
mamadoundiaye <ndiayelcoft@gmail.com>
2021-08-18 08:18:57 +0000

    first

---
+++ src/main/java/sn/modelsis/gestion_clinique/GestionCliniqueApplication.java
@@ -0,0 +1,13 @@
+package sn.modelsis.gestion_clinique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCliniqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCliniqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/modelsis/gestion_clinique/GestionCliniqueApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.modelsis.gestion_clinique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCliniqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
